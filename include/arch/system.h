d996bf0452a5cd6cc34e91d1736618848aba37c4
Adrian Astley <adastley@gmail.com>
2020-04-25 17:32:04 -0700

    Set the stack pointers for each core
    And restructure the boot code

---
+++ include/arch/system.h
@@ -0,0 +1,23 @@
+#pragma once
+
+static inline unsigned int get_core_id() {
+	unsigned int coreId;
+
+	asm volatile(
+	    "mrs	%[coreId], mpidr_el1\n"
+	    "and	%[coreId], %[coreId], #0x3"
+	    : [ coreId ] "=r"(coreId));
+
+	return coreId;
+}
+
+static inline unsigned int get_el() {
+	unsigned int el;
+
+	asm volatile(
+	    "mrs	%[el], CurrentEL\n"
+	    "lsr	%[el], %[el], #2" // Shift off the first two bits. They're always zero
+	    : [ el ] "=r"(el));
+
+	return el;
+}
--- src/kernel/kernel_main.c
+++ src/kernel/kernel_main.c
@@ -1,3 +1,4 @@
+#include "arch/system.h"
 #include "arch/timer.h"
 #include "arch/uart.h"
 #include "std_lib/atomic.h"
@@ -7,26 +8,40 @@
 static volatile uint64_t init = 0;
 static volatile uint64_t lock = 0;
 
-void kernel_main(unsigned int procId) {
-	if (procId == 0) {
-		uart_init();
-		atomic_exchange_mb_u64(&init, 1);
-	} else {
-		// Wait for the system to be initialized
-		while (atomic_compare_exchange_mb_u64(&init, 1, 1) != 1) {
-			asm volatile("nop");
-		}
+void kernel_main();
+
+void system_init() {
+	uart_init();
+
+	printf("Current EL: %u\n", get_el());
+
+	// Signal the other cores
+	atomic_exchange_mb_u64(&init, 1);
+
+	kernel_main();
+}
+
+void core_wait_for_instructions() {
+	// Wait for the system to be initialized
+	while (atomic_compare_exchange_mb_u64(&init, 1, 1) != 1) {
+		asm volatile("nop");
 	}
+
+	kernel_main();
+}
+
+void kernel_main() {
+	unsigned int coreId = get_core_id();
 
 	// Lock access to the terminal, so we don't get jumbled, interleaved words
 	while (atomic_compare_exchange_mb_u64(&lock, 0, 1) != 0) {
 		asm volatile("nop");
 	}
-	printf("Hello from processor %d\n", procId);
+	printf("Hello from core %u\n", coreId);
 	// Unlock
 	atomic_exchange_mb_u64(&lock, 0);
 
-	if (procId == 0) {
+	if (coreId == 0) {
 		while (1) {
 			uart_send(uart_recv());
 		}
